cf694e6b97d3164aa3f47a5dc09876525ccad50a
osbm <osmanfbayram@gmail.com>
2025-07-05 16:10:37 +0300

    use reader functionality

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,32 @@
-
+mod reader;
+use reader::ByteReader;
 
 fn main() {
-    // worlds/small_corruption.wld
     let path = "worlds/small_corruption.wld";
+    let bytes = std::fs::read(path).expect("Failed to read file");
+    let mut r = ByteReader::new(&bytes);
 
-    // Read the file into a vector of bytes
-    let bytes = std::fs::read(path)
-        .expect("Failed to read the file");
+    let version = r.u32();
+    println!("Version: {}", version);
 
-    println!("File size: {} bytes", bytes.len());
+    let magic = r.bytes(7);
+    println!("Magic: {}", String::from_utf8_lossy(magic));
 
-    // read first 4 bytes as u32
-    let mut header = [0u8; 4];
-    header.copy_from_slice(&bytes[0..4]);
-    let header_value = u32::from_le_bytes(header);
-    println!("Header value: {}", header_value);
+    let savefile_type = r.u8();
+    println!("Savefile type version: {}", savefile_type);
 
-    // relogic magic string is the next 7 bytes
-    let magic_string = &bytes[4..11];
-    let magic_str = String::from_utf8_lossy(magic_string);
-    println!("Magic string: {}", magic_str);
-
-    // read the next uint 1 byte
-    let version = bytes[11];
-    println!("Savefile type version: {}", version);
-
-    // revision is the next 4 bytes
-    let mut revision_bytes = [0u8; 4];
-    revision_bytes.copy_from_slice(&bytes[12..16]);
-    let revision = u32::from_le_bytes(revision_bytes);
+    let revision = r.u32();
     println!("Revision: {}", revision);
 
-    // is favorite number is the next 8 bytes
-    let is_favorite_bytes = &bytes[16..24];
-    let is_favorite = u64::from_le_bytes(is_favorite_bytes.try_into().expect("Slice with incorrect length"));
-    println!("Is favorite: {}", is_favorite);
+    let is_favorite = r.u64();
+    println!("Is Favorite: {}", is_favorite);
 
-    //         pointers = Pointers(*[f.int4() for _ in range(f.int2())])
-    let pointer_range = u16::from_le_bytes([bytes[24], bytes[25]]);
-    println!("Pointer range: {}", pointer_range);
+    let pointer_count = r.u16();
+    println!("Pointer count: {}", pointer_count);
 
-    //         pointers = Pointers(*[f.int4() for _ in range(f.int2())])
-    let mut pointers = Vec::new();
-    for i in 0..pointer_range {
-        let start = 26 + (i as usize * 4);
-        let end = start + 4;
-        let mut pointer_bytes = [0u8; 4];
-        pointer_bytes.copy_from_slice(&bytes[start..end]);
-        let pointer = u32::from_le_bytes(pointer_bytes);
-        pointers.push(pointer);
-        println!("Pointer {}: {}", i, pointer);
+    let mut pointers = vec![];
+    for _ in 0..pointer_count {
+        pointers.push(r.u32());
     }
-    println!("Total pointers: {}", pointers.len());
     println!("Pointers: {:?}", pointers);
-
-    // Read the next 4 bytes as u32
-
-
 }
