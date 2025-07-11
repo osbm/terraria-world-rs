f1d91eab3e8a67f6eb33bd3dd79084ec73b9af4f
osbm <osmanfbayram@gmail.com>
2025-07-11 17:36:46 +0300

    also save

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,17 @@ fn main() {
     let test_worlds_dir = std::env::var("TEST_WORLDS_DIR")
         .expect("TEST_WORLDS_DIR environment variable not set. Please provide the test worlds directory as a flake input.");
     println!("Using test worlds directory: {}", test_worlds_dir);
-    let world = World::from_file(&format!("{}/{}", test_worlds_dir, world_name)).expect("Failed to read world file");
+    let mut world = World::from_file(&format!("{}/{}", test_worlds_dir, world_name)).expect("Failed to read world file");
     // println!("World loaded: {:#?}", world);
     println!("Beastiary: {:#?}", world.bestiary);
     println!("Beastiary kills: {:#?}", world.bestiary.kills);
     println!("Journey Powers: {:#?}", world.journey_powers);
     println!("Chests: {:#?}", world.chests);
 
-    
+
+    println!("Writing world to file...");
+    world.save_as_wld(&format!("{}/{}", test_worlds_dir, "output.wld"))
+        .expect("Failed to write world file");
+    println!("World written successfully.");
+
 }
