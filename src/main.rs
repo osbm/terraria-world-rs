45202281547a9496065d3c7232d65de8bc67bc2a
osbm <osmanfbayram@gmail.com>
2025-07-13 21:02:06 +0300

    no need for this

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use terraria_world_parser::world::World;
 fn main() -> std::io::Result<()> {
     let test_worlds_dir = std::env::var("TEST_WORLDS_DIR")
         .expect("TEST_WORLDS_DIR environment variable not set.");
-    
+
     let world_path = format!("{}/almostemptyworld.wld", test_worlds_dir);
     println!("Loading world: {}", world_path);
-    
+
     let mut world = World::from_file(&world_path)?;
     println!("World name: {}", world.world_name);
     println!("World size: {}x{}", world.world_width, world.world_height);
-    
+
     // Print some tile information
     let mut non_empty_tiles = 0;
     for x in 0..world.world_width as usize {
@@ -25,15 +25,7 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
-    println!("Total non-empty tiles: {}", non_empty_tiles);
-    
-    // Print column sizes
-    println!("Column sizes:");
-    for (i, size) in world.get_all_column_sizes().iter().enumerate() {
-        if *size > 0 {
-            println!("Column {}: {} bytes", i, size);
-        }
-    }
-    
+
+
     Ok(())
 }
