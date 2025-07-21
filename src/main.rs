2a524357cff2f5b1be5de1dcc925864264d7fe96
osbm <osmanfbayram@gmail.com>
2025-07-21 16:33:30 +0300

    add main to test stuff

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+
+use terraria_world::world::World;
+
+fn main() {
+    // Get the path from command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path_to_wld_file>", args[0]);
+        std::process::exit(1);
+    }
+    let wld_path = &args[1];
+
+    let world = World::from_file(wld_path).expect("Failed to load world file");
+
+
+    // print the very first tile
+    println!("{:?}", world.tiles.get_tile(0, 0).unwrap());
+}
