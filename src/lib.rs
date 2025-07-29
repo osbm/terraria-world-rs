620d253cbb95e40f2fa06eeb155cd83d8ac18e82
osbm <osbm@osbm.dev>
2025-07-29 14:18:10 +0300

    export game structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,18 @@ pub mod world;
 pub mod writer;
 
 pub use world::tile::Tile;
+pub use world::World;
+
+// also export world components
+pub use world::coordinates::Coordinates;
+pub use world::enums::{LiquidType, BLOCK_TYPE_NAMES, WALL_TYPE_NAMES};
+pub use world::journey_powers::JourneyPowers;
+pub use world::sign::Sign;
+pub use world::bestiary::Bestiary;
+pub use world::environment::WorldEnvironment;
+pub use world::invasions::InvasionData;
+pub use world::npc::NPC;
+pub use world::saved_npcs::SavedNPCs;
+pub use world::tile_entity::TileEntity;
+pub use world::tile::FrameImportantData;
+pub use world::weather_events::WeatherAndEvents;
