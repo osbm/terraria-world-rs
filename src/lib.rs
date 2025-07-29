3ecef6fdc62249efa0ada9af3f120a0d04bd9f1b
osbm <osbm@osbm.dev>
2025-07-29 21:26:22 +0300

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,15 @@ pub use world::tile::Tile;
 pub use world::World;
 
 // also export world components
+pub use world::bestiary::Bestiary;
 pub use world::coordinates::Coordinates;
 pub use world::enums::{LiquidType, BLOCK_TYPE_NAMES, WALL_TYPE_NAMES};
-pub use world::journey_powers::JourneyPowers;
-pub use world::sign::Sign;
-pub use world::bestiary::Bestiary;
 pub use world::environment::WorldEnvironment;
 pub use world::invasions::InvasionData;
+pub use world::journey_powers::JourneyPowers;
 pub use world::npc::NPC;
 pub use world::saved_npcs::SavedNPCs;
-pub use world::tile_entity::TileEntity;
+pub use world::sign::Sign;
 pub use world::tile::FrameImportantData;
+pub use world::tile_entity::TileEntity;
 pub use world::weather_events::WeatherAndEvents;
